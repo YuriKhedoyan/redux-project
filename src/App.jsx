d80258708700ceb01f01af03807538271b5ecebd
YuriKhedoyan <97390659+YuriKhedoyan@users.noreply.github.com>
2023-10-10 16:30:41 +0400

    Add the project

---
+++ src/App.jsx
@@ -0,0 +1,35 @@
+import React, { useState } from 'react';
+
+import './App.css'
+
+const App = () => {
+  const [inputs, setInputs] = useState(['']);
+
+  const updateInput = (index, value) => {
+    const newInputs = [...inputs];
+    newInputs[index] = value;
+
+    if (index === inputs.length - 1 && value.trim() !== '') {
+      newInputs.push('');
+    }
+
+    setInputs(newInputs);
+  };
+
+  return (
+    <div>
+      {inputs.map((input, index) => (
+        <div key={index}>
+          <table>
+            <tr>
+              <td className='row'><input value={index + 1} disabled className='inputs' /></td>
+              <td className='input'><input key={index} type="text" value={input} onChange={(e) => updateInput(index, e.target.value)} className='inputs' /></td>
+            </tr>
+          </table>
+        </div>
+      ))}
+    </div>
+  );
+};
+
+export default App;
